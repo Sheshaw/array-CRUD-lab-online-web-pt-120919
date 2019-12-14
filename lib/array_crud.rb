24776db98110cc3c71e74c81251ce994de4445ff
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-14 07:19:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,6 +34,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  cheese = ["wow", "am", "below", "near", "arrays!"]
+  cheese[0]
   
 end
 
