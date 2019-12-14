ef05a6438ba1e0dc12884c75937b9f8d28b7b97b
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-14 07:16:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   cheese = ["high", "low", "below", "near", "arrays!"]
-  cheese.pop("arrays!")
+  cheese.pop
 end
 
 def remove_element_from_start_of_array(array)
